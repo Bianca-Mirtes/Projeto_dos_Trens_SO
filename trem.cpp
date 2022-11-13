248d872437d09fdceffdceff47f9f25a48304054
Bianca Miranda <miranda.bianca41@gmail.com>
2022-11-13 09:54:27 -0300

    Delimitação das regiões criticas

--- trem.cpp
+++ trem.cpp
@@ -17,13 +17,14 @@
 #define REGIAO_6 = 5;
 #define REGIAO_7 = 6;
 
-sem_t *semaforo_1;
-sem_t *semaforo_2;
-sem_t *semaforo_3;
-sem_t *semaforo_4;
-sem_t *semaforo_5;
-sem_t *semaforo_6;
-sem_t *semaforo_7;
+
+sem_t semaforo_1;
+sem_t semaforo_2;
+sem_t semaforo_3;
+sem_t semaforo_4;
+sem_t semaforo_5;
+sem_t semaforo_6;
+sem_t semaforo_7;
 
 // Construtor
 Trem::Trem(int ID, int x, int y) {
@@ -35,7 +36,9 @@ Trem::Trem(int ID, int x, int y) {
 
 // Função a ser executada após executar trem->START
 void Trem::run() {
-    // sem_init(semaforo_1, 0, );
+    sem_init(&semaforo_1, 0, 2);
+    sem_init(&semaforo_2, 0, 2);
+    sem_init(&semaforo_3, 0, 2);
     while (true) {
         switch (ID) {
             case 1:  // Trem 1
@@ -43,14 +46,25 @@ void Trem::run() {
                 if (velocidade == 200) {
                     break;
                 }
-                if (y == 30 && x < 470)
+                if (y == 30 && x < 470){
+                    if(x == 450){
+                        //sem_wait(&semaforo_1);
+                    }
                     x += 10;
-                else if (x == 470 && y < 230)
+                }else if (x == 470 && y < 230){
+                    if(y == 210){
+                        break;
+                    }
                     y += 10;
-                else if (x > 230 && y == 230)
+                }else if (x > 230 && y == 230){
+                    if(x == 370){
+                        //break;
+                    }
                     x -= 10;
-                else
+                }  
+                else{
                     y -= 10;
+                }
                 if (velocidade != 100) {
                     emit updateGUI(ID, x, y);  // Emite um sinal
                 }
@@ -60,14 +74,28 @@ void Trem::run() {
                 if (velocidade == 200) {
                     break;
                 }
-                if (y == 30 && x < 710)
+                if (y == 30 && x < 710){
+                    if(x == 690){
+                        //break;
+                    }
                     x += 10;
-                else if (x == 710 && y < 230)
+                }else if (x == 710 && y < 230){
+                    if(y == 210){
+                        //break;
+                    }
                     y += 10;
-                else if (x > 470 && y == 230)
+                }else if (x > 470 && y == 230){
+                    if(x == 610){
+                        //break;
+                    }
+                    if(x == 490){
+                        break;
+                    }
                     x -= 10;
-                else
+                }  
+                else{
                     y -= 10;
+                }  
                 if (velocidade != 100) {
                     emit updateGUI(ID, x, y);  // Emite um sinal para atualizar a posição do trem na tela
                 }
@@ -77,14 +105,22 @@ void Trem::run() {
                 if (velocidade == 200) {
                     break;
                 }
-                if (y == 230 && x < 350)
+                if (y == 230 && x < 350){
+                    if(x == 210){
+                        //break;
+                    }
+                    if(x == 330){
+                       //break;
+                    }
                     x += 10;
-                else if (x == 350 && y < 410)
+                }else if (x == 350 && y < 410){
                     y += 10;
-                else if (x > 110 && y == 410)
-                    x -= 10;
-                else
-                    y -= 10;
+                }else if (x > 110 && y == 410){
+                   x -= 10; 
+                }
+                else{
+                   y -= 10; 
+                }
                 if (velocidade != 100) {
                     emit updateGUI(ID, x, y);  // Emite um sinal para atualizar a posição do trem na tela
                 }
@@ -94,14 +130,25 @@ void Trem::run() {
                 if (velocidade == 200) {
                     break;
                 }
-                if (y == 230 && x < 590)
+                if (y == 230 && x < 590){
+                    if(x == 450){
+                        //break;
+                    }
+                    if(x == 570){
+                        //break;
+                    }
                     x += 10;
-                else if (x == 590 && y < 410)
-                    y += 10;
-                else if (x > 350 && y == 410)
+                }else if (x == 590 && y < 410){
+                    y += 10; 
+                }else if (x > 350 && y == 410){
+                    if(x == 370){
+                        break;
+                    }
                     x -= 10;
-                else
+                }else{
                     y -= 10;
+                }
+                    
                 if (velocidade != 100) {
                     emit updateGUI(ID, x, y);  // Emite um sinal para atualizar a posição do trem na tela
                 }
@@ -111,14 +158,21 @@ void Trem::run() {
                 if (velocidade == 200) {
                     break;
                 }
-                if (y == 230 && x < 830)
-                    x += 10;
-                else if (x == 830 && y < 410)
+                if (y == 230 && x < 830){
+                    x += 10;                    
+                }else if (x == 830 && y < 410){
                     y += 10;
-                else if (x > 590 && y == 410)
-                    x -= 10;
-                else
-                    y -= 10;
+                }else if (x > 590 && y == 410){
+                    if(x == 610){
+                        //break;
+                    }
+                    x -= 10;                    
+                }else{
+                    if(y == 250){
+                        break;
+                    }
+                    y -= 10; 
+                }   
                 if (velocidade != 100) {
                     emit updateGUI(ID, x, y);  // Emite um sinal para atualizar a posição do trem na tela
                 }
